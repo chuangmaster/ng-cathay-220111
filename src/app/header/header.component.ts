de01254985c56a016a6287130441bb64797d8dd1
chuangmaster <chuang.master@gmail.com>
2022-01-11 14:38:21 +0800

    內嵌繫結、屬性繫結、事件繫結語法

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  sitename='The Will Will Web';
-  constructor() { }
+  sitename = 'The Will Will Web';
+  constructor() {}
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.sitename = 'Hello Zhan!';
+    }, 2000);
   }
 
+  changeSiteName(event: MouseEvent) {
+    alert('you click me');
+    if (event.ctrlKey) {
+      this.sitename = 'you press ctrl and change name';
+    }
+  }
+  pressEnter(event: KeyboardEvent) {
+    alert("you press enter!");
+  }
 }
